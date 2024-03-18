81cb8bdb546c613fdfff769e0853c0635d0eb1fd
brant-matheus <brantccm@hotmail.com>
2024-03-18 16:02:01 -0300

    init grokking algorithms js

---
+++ binary-search.js
@@ -0,0 +1,12 @@
+//generating a sorted array from 1 up to size list.
+let sorted_array = []
+let size = 101
+for (let i = 1; i < size; i++){
+    sorted_array.push(i)
+}
+
+
+//binary search function.
+function binarySearch(array, item){
+    ;
+}
